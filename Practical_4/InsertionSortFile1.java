65bbfbf3f6b3732e1404613dad4dde28fbd6fe8b
Lord_Of_Victory <jnandanwar20@gmail.com>
2024-03-06 21:44:20 +0530

    Added Practical 4
    Completed Practical 4 Insertion Sort

---
+++ Practical_4/InsertionSortFile1.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class InsertionSortFile1 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        FileReader f = new FileReader("./sequentialUnrepeatedNumbers.txt");
+
+        Scanner fileScanner = new Scanner(f);
+        int[] array = new int[100001]; // maximum of 100000 elements in the file
+        int size = 0;
+
+        while (fileScanner.hasNextInt()) {
+            array[size++] = fileScanner.nextInt();
+        }
+        fileScanner.close();
+
+        long start = System.currentTimeMillis();
+        insertionSort(array);
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+
+        System.out.println("\nTime taken for sorting: " + timeElapsed + " ms");
+
+        FileWriter writer = new FileWriter("./sequentialUnrepeatedNumbersSortedOutput.txt");
+        for (int i = 0; i < size; i++) {
+            writer.write(array[i]+"\n");
+        }
+        writer.close();
+
+        scanner.close();
+    }
+
+    public static void insertionSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
